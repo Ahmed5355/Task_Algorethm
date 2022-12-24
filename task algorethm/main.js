517fcc23dbd679e4edb63afe560663d4d475548d
Ahmed Ibrahim <82470677+Ahmed5355@users.noreply.github.com>
2022-12-24 15:42:06 +0200

    Add files via upload

---
+++ task algorethm/main.js
@@ -0,0 +1,62 @@
+let arr = [1,3,2,6,4,5];
+
+
+// ================================================
+// =============  Insertion Sort  =================
+// ================================================
+function insrtionSort(){
+    for(let x = 0; x < arr.length;x++){
+    let key = arr[x];
+    let y= x-1;
+    
+    while(y >= 0 && arr[y] > key){
+        arr[y+1] = arr[y];
+        y--;
+    }
+    arr[y + 1] = key
+}
+return arr
+}
+
+console.log("Sorting By InsertionSort : ",insrtionSort(arr))
+
+
+
+
+
+
+
+
+
+
+// ================================================
+// =============  Merge Sort  =====================
+// ================================================
+function mergeSort(arr) {
+    const half = arr.length / 2
+
+    if(arr.length < 2){
+        return arr 
+    }
+    
+    const left = arr.splice(0, half)
+    return merge(mergeSort(left),mergeSort(arr))
+}
+
+function merge(left, right) {
+    let arr = []
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            arr.push(left.shift())  
+        } else {
+            arr.push(right.shift()) 
+        }
+    }
+    return [ ...arr, ...left, ...right ]
+}
+
+console.log("Sorting By MergeSort : ",mergeSort(arr));
+
+
+
+
